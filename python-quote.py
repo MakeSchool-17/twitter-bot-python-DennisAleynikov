b554208a3568e84712e2297f7b2582d610cf29c2
Dennis Aleynikov <dennis.aleynikov@students.makeschool.com>
2015-09-28 01:18:07 -0700

    Added comments to python_quote

--- python-quote.py
+++ python-quote.py
@@ -1,14 +1,16 @@
 import random
 
+# A tuple containing the quotes and easily accessible
 quotes = ('It\'s just a flesh wound.',
           'He\'s not the Messiah. He\'s a very naughty boy!',
           'THIS IS AN EX-PARROT!!')
 
-
+# Returns a random quote from the quote tuple
 def random_python_quote():
     rand_index = random.randint(0, len(quotes) - 1)
     return quotes[rand_index]
 
 if __name__ == '__main__':
+    # Get a quote and print it
     quote = random_python_quote()
     print(quote)
