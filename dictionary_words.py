24176b0416be01410f423ab6596b50799ade792c
Dennis Aleynikov <dennis.aleynikov@students.makeschool.com>
2015-09-28 01:15:40 -0700

    Added descriptions

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,6 +1,7 @@
 import random
 import sys
 
+# Function for making a capitalized sentence of len words out of wordlist dict
 def random_sentence(dict, len):
     words = random.sample(dict, len - 1)
     sentence = " " + " ".join(words) + "."
@@ -9,7 +10,9 @@ def random_sentence(dict, len):
 
 
 if __name__ == '__main__':
+    # Obtain list of words from system wordlist
     dictionary = open('/usr/share/dict/words').read().splitlines()
+    # Get length of generated "sentence"
     length = int(sys.argv[1])
     sentence = random_sentence(dictionary,length)
     print(sentence)
